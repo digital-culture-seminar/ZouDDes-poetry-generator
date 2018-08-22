1c54123e968155a8ec3654105d00e41d45dc20d4
ZouDDes <42592706+ZouDDes@users.noreply.github.com>
2018-08-21 20:49:33 -0500

    first spyder python
    first spyder python script

---
+++ Spyder_helloWorld.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Aug. 21, Tuesday
+Author Jun Zou
+License: ZZ-JJ-88
+Descriptions: a simple python script
+
+This is a temporary script file.
+"""
+# set variables
+world_adjective = "new"
+
+# print statement
+print "hello {adj} world".format(adj=world_adjective)
