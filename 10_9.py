0d6bf3036684f2d5a7c55525080a0146f5ae1af6
ZouDDes <42592706+ZouDDes@users.noreply.github.com>
2018-10-16 13:39:04 -0500

    P2

---
+++ 10_9.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 18 20:37:34 2018
+
+script color and feeling poem
+@author: junjiang
+liense 
+"""
+
+mylist = []
+for i in range(0,5):
+    mylist.append("hello world " + str(i))
+    
+    
+
+
+#with open("poem.md", "w") as p:
+#    for i in range(1,10):
+#        poem = "hello world " + str(i)
+#        print poem
+#        p.write(poem)
+#        p.write("\n")
+   
+#poem = "hello world"
+#
+###print poem
+##print "hello \n world"
+##
+##print """hello
+##world
+##x2"""
+##
+##with open("poem.md", "w") as p:
+##    p.write("## Title")
+##    p.write("\n")
+##    p.write("'''")
+##    p.write("\n")
+##    p.write(poem)
+##    p.write("\n")
+##    p.write("'''")
+#    
+#with open("poem.md", "w") as p:
+#    p.write("""
+### Title
+#'''
+#
+#{poem}
+#'''
+#""".format(poem=poem))
+    
+    
+
---
+++ poem.md
@@ -0,0 +1,6 @@
+Goethe orange mystery rose and sights are deep.
+Drawing the conversation out so long that the sender never intended or imagined.
+A suprisingly large number of people will believe that you are taking pains to preserve the author's intent.
+Email is sent to the very heart of the automated office.
+PEOPLE INSIDE THE MONITOR: You get 3 opportunities to advertise your Rock band, no more.
+It should be attributed to the bottom of the metaphor, rhetoric, and invective which pass daily through the mail.
